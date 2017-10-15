8213bd338809c6a18dba804a36bf537af95707cb
Ger Hobbelt <ger@hobbelt.com>
2017-10-15 02:21:32 +0200

    synchronized with monorepo JISON

--- ebnf-parser-prelude.js
+++ ebnf-parser-prelude.js
@@ -1,10 +1,3 @@
 
-import XRegExp from '@gerhobbelt/xregexp';       // for helping out the `%options xregexp` in the lexer
-import helpers from 'jison-helpers-lib';
-import fs from 'fs';
-import transform from './ebnf-transform';
-
 // hack:
 var assert;
-
-// end of prelude
